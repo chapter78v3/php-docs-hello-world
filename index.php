699c39dfd3680570965ce8b72497ae92fec5adf5
chapter78v3 <milan_chappell@yahoo.com>
2022-08-28 17:57:00 -0400

    revert

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php
 
-echo "Hello World!/n Welcome to 'Chappell Secure Technology Solutions'/n Our Website is currently being developed/n If you have questions please contact us @ mchappell@chappellsecuretechnology.com";
-?>
+echo "Hello World!;
