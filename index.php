b7efbd73632cb56f249a5e14f8b6db306a6188ae
chapter78v3 <milan_chappell@yahoo.com>
2022-08-28 17:54:18 -0400

    spell check

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 
-echo "Hello World!/n Welcom to "Chappell Secure Technology Solutions"/n Our Website is currently being developed/n If you have questions please contact us @ mchappell@chappellsecuretechnology.com";
+echo "Hello World!/n Welcome to "Chappell Secure Technology Solutions"/n Our Website is currently being developed/n If you have questions please contact us @ mchappell@chappellsecuretechnology.com";
 ?>
